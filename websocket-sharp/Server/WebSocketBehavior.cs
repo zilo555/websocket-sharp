a0d86f2adf29726388c37590d911dd66735533a9
sta <sta.blockhead@gmail.com>
2025-11-07 22:12:12 +0900

    [Modify] Polish it

--- websocket-sharp/Server/WebSocketBehavior.cs
+++ websocket-sharp/Server/WebSocketBehavior.cs
@@ -1445,7 +1445,7 @@ namespace WebSocketSharp.Server
     protected void SendAsync (FileInfo fileInfo, Action<bool> completed)
     {
       if (_websocket == null) {
-        var msg = "The SendAsync method is not available.";
+        var msg = "The session has not started yet.";
 
         throw new InvalidOperationException (msg);
       }
