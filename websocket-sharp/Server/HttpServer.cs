2c56249573531519b6f0f3a53fb636d359238ba3
sta <sta.blockhead@gmail.com>
2026-08-02 16:32:52 +0900

    [Modify] Throw it

--- websocket-sharp/Server/HttpServer.cs
+++ websocket-sharp/Server/HttpServer.cs
@@ -345,8 +345,11 @@ namespace WebSocketSharp.Server
 
       set {
         lock (_sync) {
-          if (!canSet ())
-            return;
+          if (!canSet ()) {
+            var msg = "The current state of the server is neither Ready nor Stop.";
+
+            throw new InvalidOperationException (msg);
+          }
 
           _listener.AuthenticationSchemes = value;
         }
